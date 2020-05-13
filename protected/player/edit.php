093062b02f6f05fcb561b58d680e4c1dcd656a58
Bencsik Krisztián <bencsikkrisztian1999@gmail.com>
2020-05-13 15:46:34 +0200

    Player funkciók + css
    Működnek a player funkciók, és kis külső javítás

--- protected/player/edit.php
+++ protected/player/edit.php
@@ -6,6 +6,7 @@
 else: 
 	if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['editPlayer'])) {
 		$postData = [
+			'id' => $_POST['playerId'],
 			'name' => $_POST['name'],
 			'club' => $_POST['club'],
 			'nationality' => $_POST['nationality'],
@@ -29,7 +30,8 @@ else:
 					':league' => $postData['league'],
 					':pace' => $postData['pace'],
 					':shooting' => $postData['shooting'],
-					':defending' => $postData['defending']
+					':defending' => $postData['defending'],
+					':id' => $postData['id']
 				];
 				require_once DATABASE_CONTROLLER;
 				if(!executeDML($query, $params)) {
@@ -45,64 +47,79 @@ else:
 	if(empty($player)) :
 		header('Location: index.php');
 		else : ?>
-		<form method="post">
+		<table id="keret" align="center" width="60%" border="4pt" cellpadding="20pt">
+		<tr><td>
+	<form method="post">
+		<h1><b>Játékos szerkesztése</b></h1>
+		<input type="hidden" name="playerId" value="<?=$player['id'] ?>">
 		<div class="form-row">
 			<div class="form-group col-md-12">
-				<label for="playerName">Név</label>
+				<label for="playerName"><b>Név</b></label>
 				<input type="text" class="form-control" id="playerName" name="name">
 			</div>
 			<div class="form-group col-md-12">
-				<label for="playerCLub">Klub</label>
+				<label for="playerCLub"><b>Klub</b></label>
 				<input type="text" class="form-control" id="playerClub" name="club">
 			</div>
 		</div>
 
 				<div class="form-row">
 			<div class="form-group col-md-12">
-				<label for="playerNationality">Nemzetiség</label>
+				<label for="playerNationality"><b>Nemzetiség</b></label>
 				<input type="text" class="form-control" id="playerNationality" name="nationality">
 			</div>
 		</div>
 
 		<div class="form-row">
 			<div class="form-group col-md-12">
-				<label for="playerLeague">Liga</label>
+				<label for="playerLeague"><b>Liga</b></label>
 				<select class="form-control" id="playerLeague" name="league">
 		      		<option value="0">Premier League</option>
 		      		<option value="1">LaLiga</option>
 		      		<option value="2">Serie A</option>
 		      		<option value="3">Bundesliga</option>
-		      		<option value="4">Bundesliga</option>
-		      		<option value="5">Más liga...</option>
+		      		<option value="4">Ligue 1</option>
+		      		<option value="5">Egyéb liga</option>
 		    	</select>
 			</div>
 		</div>
 		
 		<div class="form-row">
 			<div class="form-group col-md-12">
-		    	<label for="playerPace">Gyorsaság (1-99)</label>
+		    	<label for="playerPace"><b>Gyorsaság (1-99)</b></label>
 				<input type="number" class="form-control" id="playerPace" name="pace">
 		  	</div>
 		</div>
 
 				<div class="form-row">
 			<div class="form-group col-md-12">
-		    	<label for="playerShooting">Lövés (1-99)</label>
+		    	<label for="playerShooting"><b>Lövés (1-99)</b></label>
 				<input type="number" class="form-control" id="playerShooting" name="shooting">
 		  	</div>
 		</div>
 
 				<div class="form-row">
 			<div class="form-group col-md-12">
-		    	<label for="playerDefending">Védekezés (1-99)</label>
+		    	<label for="playerDefending"><b>Védekezés (1-99)</b></label>
 				<input type="number" class="form-control" id="playerDefending" name="defending">
 		  	</div>
 		</div>
 
 
 
-		<button type="submit" class="btn btn-primary" name="addPlayer">Mentés</button>
-			</form>
+		<button type="submit" class="btn btn-primary" name="editPlayer">Mentés</button>
+	</form>
+	</td>
+	<td>
+		<p><b>Név</b> - A játékos neve, maximum 64 karakter</p>
+		<p><b>Klub</b> - A játékos jelenlegi csapataának neve, maximum 64 karakter</p>
+		<p><b>Nemzetiség</b> - Az ország ahonnan származik a játékos, maximum 64 karakter</p>
+		<p><b>Liga</b> - Az 5 legjobb ligában, vagy egyéb ligában játszik a játékos?</p>
+		<p><b>Gyorsaság</b> - A játékos gyorsasága, 1 és 99 közötti érték</p>
+		<p><b>Lövés</b> - A játékos rúgási képessége, 1 és 99 közötti érték</p>
+		<p><b>Védekezés</b> - A játékos védekezési képessége,1 és 99 közötti érték</p>
+	</td>
+	</table>
 		<?php endif;
 	endif;
 endif;
